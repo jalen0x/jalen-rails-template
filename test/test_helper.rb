2cccf3579adbf07e0c403a24c866a1155e5d98f9
Jalen Yan <jalenx.me@outlook.com>
2026-05-03 01:39:32 +0800

    test: add factory bot baseline

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,16 +3,15 @@ require_relative "../config/environment"
 require "rails/test_help"
 require_relative "support/confidence_check"
 
+Capybara.configure do |config|
+  config.test_id = "data-testid"
+end
+
 module ActiveSupport
   class TestCase
     include TestSupport::ConfidenceCheck
 
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
